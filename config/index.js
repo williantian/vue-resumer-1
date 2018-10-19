93308dd63d97fa7a01aa76c471340a695e6b7e83
willian <williantian1995@gmail.com>
2018-10-19 18:46:27 +0800

    dist

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'vue-resume-1/dist/',
+    assetsPublicPath: '/',
 
     /**
      * Source Maps
